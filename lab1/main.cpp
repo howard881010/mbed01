f38f16c2033999eabf72e2dee34bff81a17f361c
107061240 <howard10532@gmail.com>
2021-03-02 23:44:38 -0800

    add main.cpp

---
+++ lab1/main.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+
+DigitalOut myLED(LED1);
+DigitalOut myLED2(LED3);
+
+void Led(DigitalOut &ledpin, int n);
+
+int main()
+{
+   myLED = 1;
+   myLED2 = 1;
+   while (true)
+   {
+      Led(myLED,4);
+      Led(myLED2,6);
+   }
+}
